a3c53660baaaba2fb8407fc2c503187967708137
ritu1512 <ritugaba123@gmail.com>
2024-07-30 21:44:07 -0400

    EDA nad Problems Statement

--- src/logger.py
+++ src/logger.py
@@ -7,13 +7,23 @@ logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
 os.makedirs(logs_path, exist_ok = True)
 
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+# create a custom logger
+logger = logging.getLogger(__name__)
+# set the overall logging levvel
+logger.setLevel(logging.INFO)
 
-logging.basicConfig(
-    filename=LOG_FILE_PATH, 
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s", 
-    level = logging.INFO,
+# CREATE a file handlers
+file_handler = logging.FileHandler(LOG_FILE_PATH)
+console_handler = logging.StreamHandler()
+# create formaters and add it to the handlers
+formatter = logging.Formatter("[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
 
-)
+#  add handler to the logger
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+
 
 if __name__ == "__main___":
-    logging.info("logging has statted")
+    logging.info("logging has startted")
