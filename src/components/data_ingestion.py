abd9b4288165b1676763fa804c0f649bc6262686
ritu1512 <ritugaba123@gmail.com>
2024-08-07 15:32:44 -0400

    Hyper patrameter training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -57,8 +57,8 @@ if __name__ =="__main__":
     train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
 
 
-    modeltrainer = ModelTrainer()
-    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
 
 
--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import pandas as pd
 import dill
 import pickle
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 
 
 from src.exception import CustomException
@@ -19,11 +19,19 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train, X_test,  y_test, models):
+def evaluate_models(X_train, y_train, X_test,  y_test, models, param):
     try:
         report = {}
         for model_name, model in models.items():
-            model.fit(X_train, y_train)  # train model
+            # model.fit(X_train, y_train)  # train model
+            para=param[model_name]
+
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
