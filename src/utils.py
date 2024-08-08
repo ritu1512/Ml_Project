60768006bb89832757dda41082a799a1a50de2d0
ritu1512 <ritugaba123@gmail.com>
2024-08-08 10:08:25 -0400

    Prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -15,7 +15,7 @@ def save_object(file_path, obj):
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path, exist_ok=True)
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
+            pickle.dump(obj, file_obj)
     except Exception as e:
         raise CustomException(e, sys)
     
@@ -45,6 +45,18 @@ def evaluate_models(X_train, y_train, X_test,  y_test, models, param):
         return report
     except Exception as e:
         raise CustomException(e,sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+            
+    except Exception as e:
+        raise CustomException(e,sys)
+        
+
+    
 
 
 
